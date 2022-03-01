a4543f219d4ce3a623c0ee87d7ea3be74e7c95f3
Kofi Poku <kap029@bucknell.edu>
2022-03-01 00:11:53 -0500

    message

--- coaster_control/coaster_control.ino
+++ coaster_control/coaster_control.ino
@@ -9,43 +9,53 @@
  * Only valid states (after initial state of 1001) are 1010 (10) and 0101 (5)
  */
 
-char sensors[4];
-char zones[4];
+char sensors[5];
+char zones[5];
 
-const int SOLENOID_0 = 10;
-const int SOLENOID_1 = 11;
-const int SOLENOID_2 = 12;
-const int SOLENOID_3 = 13;
+const int SOLENOID_0 = 6;
+const int SOLENOID_1 = 10;
+const int SOLENOID_2 = 11;
+const int SOLENOID_3 = 9;
 
 // Sensor really refers to the IR transmitter and receiver acting in tandem (so their output)
-const int SENSOR_0 = 4; // sensor 0
-const int SENSOR_1 = 5; // sensor 1
-const int SENSOR_2 = 6; // sensor 2
-const int SENSOR_3 = 7; // sensor 3
+// const int SENSOR_0 = 4; // sensor 0
+// const int SENSOR_1 = 5; // sensor 1
+// const int SENSOR_2 = 6; // sensor 2
+// const int SENSOR_3 = 7; // sensor 3
+
+const int threshold = 64;
 
 int initial_state = 1;
 
+int sensor0;
+int sensor1;
+int sensor2;
+int sensor3;
+
+int something = 0;
 void setup() {
   pinMode(SOLENOID_0, OUTPUT);
   pinMode(SOLENOID_1, OUTPUT);
   pinMode(SOLENOID_2, OUTPUT);
   pinMode(SOLENOID_3, OUTPUT);
   
-  pinMode(SENSOR_0, INPUT);
-  pinMode(SENSOR_1, INPUT);
-  pinMode(SENSOR_2, INPUT);
-  pinMode(SENSOR_3, INPUT);
+  pinMode(A0, INPUT); // sensor 0
+  pinMode(A1, INPUT); // sensor 1
+  pinMode(A2, INPUT); // sensor 2
+  pinMode(A3, INPUT); // sensor 3
 
-  attachInterrupt(digitalPinToInterrupt(2), emergencyStop, RISING); 
-  attachInterrupt(digitalPinToInterrupt(3), sensorInterrupt, FALLING); 
+//  attachInterrupt(digitalPinToInterrupt(2), emergencyStop, RISING); 
+//  attachInterrupt(digitalPinToInterrupt(3), sensorInterrupt, FALLING); 
 
-  // zones = "1001"; 
+  strcpy(sensors, "1001");
+  strcpy(zones, "1001");
   // update_sensors(*sensors);
+  
 
   Serial.begin(9600);
 }
 
-int emergencyStop() {
+void emergencyStop() {
   digitalWrite(SOLENOID_0, LOW);
   digitalWrite(SOLENOID_1, LOW);
   digitalWrite(SOLENOID_2, LOW);
@@ -54,78 +64,138 @@ int emergencyStop() {
   // Currently staying in the while loop, no recovery from an emergency stop
   // TODO: Add an emergency state and set it so maybe we can return control to loop()
   while (true) {
+    Serial.print("Emergency zones: ");
+    Serial.println(zones);
     Serial.println("Emergency Stop Forever");
   }
 }
 
 // So do we want to just continuously read sensors OR do we want to wait until a state change in one of the sensors?
-int sensorInterrupt() {
+void sensorInterrupt() {
 
 }
 
-}
-int update_zones(){
+void update_zones(){
   // The zones are kind of like the previous/current state of the sensors?
-  zones = sensors;
+  if (strcmp(sensors, "1010") == 0) {
+    initial_state = 0;
+    strcpy(zones, "1010");    
+  }  
+  else if (strcmp(sensors, "0101") == 0) {
+    initial_state = 0;
+    strcpy(zones, "0101");
+  }
+  else if (!initial_state && strcmp(sensors, "1001")) {
+    emergencyStop();
+  }
+  else if (strcmp(sensors, "1001") == 0) {
+    emergencyStop();
+  }
+  else {
+    return;
+  }
 }
 
-int update_sensors() {
-  int sensor0 = digitalRead(SENSOR_0);
-  int sensor1 = digitalRead(SENSOR_1);
-  int sensor2 = digitalRead(SENSOR_2);
-  int sensor3 = digitalRead(SENSOR_3);
+void update_sensors() {
 
-  if (sensor0 == 0)
+  sensor3 = analogRead(A3);
+  sensor2 = analogRead(A2);
+  sensor1 = analogRead(A1);
+  sensor0 = analogRead(A0);  
+  
+
+  Serial.print("Sensor 0: ");
+  Serial.print(sensor0);
+  Serial.print(" | Sensor 1: ");
+  Serial.print(sensor1);
+  Serial.print(" | Sensor 2: ");
+  Serial.print(sensor2);
+  Serial.print(" | Sensor 3: ");
+  Serial.println(sensor3);
+
+  if (sensor0 > threshold)
     sensors[0] = '0';
   else
     sensors[0] = '1';
 
-  if (sensor1 == 0)
-    sensor[1] = '0';
+  if (sensor1 > threshold)
+    sensors[1] = '0';
   else
-    sensor[1] = '1';
+    sensors[1] = '1';
 
-  if (sensor2 == 2)
-    sensor[2] = '0';
+  if (sensor2 > threshold)
+    sensors[2] = '0';
   else
-    sensor[2] = '1';
+    sensors[2] = '1';
 
-  if (sensor3 == 0)
-    sensor[3] = '0';
+  if (sensor3 > threshold)
+    sensors[3] = '0';
   else
     sensors[3] = '1';
+
+  sensors[4] = '\0';
 
   update_zones();
 }
 
 void loop() {
-  update_sensors();
+
+  Serial.print("Current sensors state: ");
+  Serial.println(sensors);
+  Serial.print("Current zone state: ");
+  Serial.println(zones);
+  Serial.print("Initial state: ");
+  Serial.println(initial_state);
+  
+
+//  digitalWrite(SOLENOID_0, HIGH); // I'm actually not too sure which solenoids correspond to which breaks
+//  // TODO: Figure out order of solenoids, sensors, and coaster cars 
+//  digitalWrite(SOLENOID_1, HIGH);
+//  delay(1000);
+//  digitalWrite(SOLENOID_0, LOW);
+//  digitalWrite(SOLENOID_1, LOW);
+//  delay(1000);
+//  
+//  digitalWrite(SOLENOID_2, LOW);
+//  
+//  digitalWrite(SOLENOID_3, LOW);
   
   if (initial_state) {
-    if (!zones.equals("1001")) {
+    if (strcmp(zones, "1001") != 0) {
       // TODO: figure out how best to handle
+      Serial.println("stop in initial state check");
       emergencyStop();
     } else {
+      Serial.println("Currently about to release breaks..");
       // release break to get to "1010"
-      digitalWrite(SOLENOID_0); // I'm actually not too sure which solenoids correspond to which breaks
+      digitalWrite(SOLENOID_0, HIGH); // I'm actually not too sure which solenoids correspond to which breaks
       // TODO: Figure out order of solenoids, sensors, and coaster cars 
+      digitalWrite(SOLENOID_1, LOW);
+      digitalWrite(SOLENOID_2, LOW);
+      digitalWrite(SOLENOID_3, LOW);
     }
   }
-  if (zones.equals("1010")) {
+  else if (strcmp(zones,"0101")) {
     // release breaks to get to "0101"
     // Try to release the "forward" car first as a bit of a safety measure
-    digitalWrite(SOLENOID_1); // Not sure which solenoids correspond to which breaks
-    digitalWrite(SOLENOID_3);
+    digitalWrite(SOLENOID_0, LOW);
+    digitalWrite(SOLENOID_2, LOW);
+    digitalWrite(SOLENOID_1, HIGH); // Not sure which solenoids correspond to which breaks
+    digitalWrite(SOLENOID_3, HIGH);
   }
-  if (zones.equals("0101")) {
+  else if (strcmp(zones, "1010")) {
     // release break to get to "1010"
     // Try to release the "forward" car first as a bit of a safety measure
-    digitalWrite(SOLENOID_0); // Not sure which solenoids correspond to which breaks
-    digitalWrite(SOLENOID_2);
+    digitalWrite(SOLENOID_1, LOW);
+    digitalWrite(SOLENOID_3, LOW);
+    digitalWrite(SOLENOID_0, HIGH); // Not sure which solenoids correspond to which breaks
+    digitalWrite(SOLENOID_2, HIGH);
   }
   else {
     // add code to handle error gracefully and allow system to recover 
     // Just shutting down for now
+    Serial.println("stop in else block");
     emergencyStop();
   }
+  update_sensors();
 }
--- solenoid_test/solenoid_test.ino
+++ solenoid_test/solenoid_test.ino
@@ -1,15 +1,19 @@
 
 
 const int buttonPin = 8;     // the number of the pushbutton pin
-const int solenoidOnePin = 9;      // the number of the solenoid pin
-const int solenoidTwoPin = 10;
+const int solenoid0Pin = 6;      // the number of the solenoid pin
+const int solenoid1Pin = 9;
+const int solenoid2Pin = 10;
+const int solenoid3Pin = 11;
 
 
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
-  pinMode(solenoidOnePin, OUTPUT);
-  pinMode(solenoidTwoPin, OUTPUT);
+//  pinMode(solenoid0Pin, OUTPUT);
+//  pinMode(solenoid1Pin, OUTPUT);
+  pinMode(solenoid2Pin, OUTPUT);
+//  pinMode(solenoid3Pin, OUTPUT);
   
   pinMode(buttonPin, INPUT);  // i guess button
   Serial.begin(9600);
@@ -20,19 +24,32 @@ void loop() {
   // put your main code here, to run repeatedly:
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
-
   // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
   if (buttonState == HIGH) {
     // turn LED on:
-    digitalWrite(solenoidOnePin, HIGH);
-    digitalWrite(solenoidTwoPin, HIGH);
+//    digitalWrite(solenoid0Pin, HIGH);
+//    digitalWrite(solenoid1Pin, HIGH);
+    digitalWrite(solenoid2Pin, HIGH);
+//    digitalWrite(solenoid3Pin, HIGH);
     Serial.println("Button pushed");
     Serial.println();
   } else {
     // turn LED off:
-    digitalWrite(solenoidOnePin, LOW);
-    digitalWrite(solenoidTwoPin, LOW);
+//    digitalWrite(solenoid0Pin, LOW);
+//    digitalWrite(solenoid1Pin, LOW);
+    digitalWrite(solenoid2Pin, LOW);
+//    digitalWrite(solenoid3Pin, LOW);
     Serial.println("Button NOT pushed");
     Serial.println();
   }
+//  digitalWrite(solenoid0Pin, HIGH);
+//  digitalWrite(solenoid1Pin, HIGH);
+//  digitalWrite(solenoid2Pin, HIGH);
+//  digitalWrite(solenoid3Pin, HIGH);
+//  delay(5);
+//  digitalWrite(solenoid0Pin, LOW);
+//  digitalWrite(solenoid1Pin, LOW);
+//  digitalWrite(solenoid2Pin, LOW);
+//  digitalWrite(solenoid3Pin, LOW);
+//  delay(15);
 }
